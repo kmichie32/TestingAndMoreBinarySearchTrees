7556e38fab3369b0d50466592436edb5db492cb8
Kevin Michie <kmichie32@gmail.com>
2016-05-02 17:01:54 -0500

    Practice with Unit Testing
    This is from HackerRank Tutorial

---
+++ src/com/kevinmichie/Tree.java
@@ -0,0 +1,8 @@
+package com.kevinmichie;
+
+public interface Tree<D extends Comparable> {
+    public boolean isEmpty();
+    public int cardinality();
+    public boolean member(D elt);
+    public NonEmptyBST<D> add(D elt);
+}
